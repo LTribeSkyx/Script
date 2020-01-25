56ebfff7882795f376dc3054c24c99359ce22508
LTribeSkyx <60221182+LTribeSkyx@users.noreply.github.com>
2020-01-26 00:52:09 +0800

    Create wywnds.js

---
+++ wywnds.js
@@ -0,0 +1,17 @@
+  
+// 微信公众号@QTribe
+// Quantumult X 脚本
+
+/*
+^https://p\.du\.163\.com/readtime/info.json url reject
+^https:\/\/p\.du\.163\.com\/gain\/readtime\/info\.json url script-response-body https://raw.githubusercontent.com/LTribeSkyx/Script/master/wywnds.js
+hostname = p.du.163.com,
+*/
+
+var body = $response.body;
+var tribe = JSON.parse(body);
+tribe.tradeEndTime = 4070957802000;
+body = JSON.stringify(tribe);
+$done({body});
+
+// From QTribe
