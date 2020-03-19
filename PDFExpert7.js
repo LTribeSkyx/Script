6c2336117da5caa756e2effa9cbe1765c10cad70
LTribeSkyx <60221182+LTribeSkyx@users.noreply.github.com>
2020-03-19 16:12:50 +0800

    updata

--- PDFExpert7.js
+++ PDFExpert7.js
@@ -1,16 +1,20 @@
-// 微信公众号@QTribe
-// Quantumult X 脚本
-
 /*
 
-^https:\/\/license\.pdfexpert\.com\/api\/1.0\/pdfexpert6\/subscription\/(refresh|check) url script-response-body https://raw.githubusercontent.com/LTribeSkyx/Script/master/PDFExpert7.js
+Quantumult X
+
+PDF EXPERT 7 Download Link：http://t.cn/A6zQCfn6
+
+[rewrite_local]
+https?:\/\/license\.pdfexpert\.com\/api\/1.0\/pdfexpert6\/subscription\/(check|refresh) url script-response-body PDFExpert.js
+
+[mitm]
 hostname = license.pdfexpert.com,
 
-*/  
+*/
 
-var tribe = JSON.parse($response.body);
+let obj = JSON.parse($response.body);
 
-tribe = {
+obj = {
   "isEligibleForIntroPeriod":true,
   "originalTransactionId":"730000355000000",
   "subscriptionExpirationDate":"01:01 01/01/2020",
@@ -21,6 +25,6 @@ tribe = {
   "isInGracePeriod":true
 };
 
-$done({body: JSON.stringify(tribe)});
+$done({body: JSON.stringify(obj)});
 
 // From QTribe
