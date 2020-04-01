3c240c073e246bee54dad6d50587ba7538349a6f
LTribeSkyx <60221182+LTribeSkyx@users.noreply.github.com>
2020-04-01 14:29:14 +0800

    Create

---
+++ tanqinba.js
@@ -0,0 +1,22 @@
+/*
+
+Quantumult X 脚本:
+tanqinba Unlock（by LTribe）
+tanqinba Download Link : http://t.cn/A6zhp3Bm
+
+[rewrite_local]
+# tanqinba Unlock （by LTribe）
+^http:\/\/www\.tan8\.com\/codeindex\.php\?d\=common\&c* url script-response-body tanqinba.js
+
+[mitm]
+hostname = www.tan8.com,
+
+*/
+
+let obj = JSON.parse($response.body);
+var vid[] = obj.data.result.videoVid;
+Delete obj.data.result.alertDic;
+obj.data.result.statusCode = "1";
+obj.data.result.message = "Success";
+obj.data.result.videoUrl = "http:\/\/videooss.tan8.com\/tan8video\/" + vid[0] + vid[1] + "\/" +vid[-2] + vid[-1] +"\/" + vid + "\/" + vid + "_2.mp4";
+$done({body: JSON.stringify(obj)});
