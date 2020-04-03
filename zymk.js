ec2702eae5aaf90fcbae45ddec7ce63848e7d3af
LTribeSkyx <60221182+LTribeSkyx@users.noreply.github.com>
2020-04-04 02:34:36 +0800

    update

--- zymk.js
+++ zymk.js
@@ -1,17 +1,45 @@
-  
-// 微信公众号@QTribe
-// Quantumult X 脚本
-
 /*
-^https:\/\/getuserinfo\-globalapi\.zymk\.cn\/app\_api\/v5\/getuserinfo\/ url script-response-body https://raw.githubusercontent.com/LTribeSkyx/Script/master/zymk.js
-hostname = getuserinfo-globalapi.zymk.cn,
+
+Quantumult X 脚本:
+知音漫客解锁会员VIP及付费漫画 （by LTribe）
+Download Link : http://t.cn/A6PaJVrT
+
+[rewrite_local]
+# 知音漫客解锁会员VIP及付费漫画 （by LTribe）
+^https?:\/\/.*\.zymk\.cn\/app_api\/v\d\/(getuserinfo|purchase_chapters_coin)\/$ url script-response-body zymk.js
+
+[mitm]
+hostname = *.zymk.cn,
+
 */
 
-var body = $response.body;
-var tribe = JSON.parse(body);
-tribe.Uviptime = 4070957802000;
-tribe.isvip = 1;
-body = JSON.stringify(tribe);
-$done({body});
+let obj = JSON.parse($response.body);
+const path1 = "/getuserinfo/";
+const path2 = "/purchase_chapters_coin/";
 
-// From QTribe
+if ($request.url.indexOf(path1) != -1){
+    obj.data.Uviptime = 4102419417000;
+    obj.data.Cnewviptime = 4102419417000;
+    obj.data.isvip = 1;
+    obj.data.coins = 99999;
+    obj.data.Cgold = 99999;
+    obj.data.Cticket = 99999;
+    obj.data.recommend = 99999;
+    obj.data.vipdays = 9999;
+    obj.data.Ulevel = 9;
+ };
+ 
+
+if ($request.url.indexOf(path2) != -1){
+   obj.status = 1;
+   obj.msg = "ok";
+   obj.data.Cnewviptime = 4102419417000;
+   obj.data.isvip = 1;
+   obj.data.coins = 99999;
+   obj.data.Cgold = 99999;
+   obj.data.Cticket = 99999;
+   obj.data.recommend = 99999;
+   obj.data.vipdays = 99999;
+ };
+ 
+ $done({body: JSON.stringify(obj)});
