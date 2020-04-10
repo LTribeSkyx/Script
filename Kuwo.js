f59a383eb893fa3454a102d8a1c5eaff6c137ee8
LTribeSkyx <60221182+LTribeSkyx@users.noreply.github.com>
2020-04-11 00:39:54 +0800

    update

--- Kuwo.js
+++ Kuwo.js
@@ -15,25 +15,22 @@ hostname = *.kuwo.cn,
 
 var obj = JSON.parse($response.body);
 
-const vip = '/vip/v2/user/vip';
-const time = '/vip/spi/mservice';
+const path1 = "/vip/v2/user/vip";
+const path2 = "/vip/spi/mservice";
 
-if ($request.url.indexOf(vip) != -1) {
+if ($request.url.indexOf(path1) != -1) {
 	obj.data.isNewUser = "2";
-  obj.data.isYearUser = "2";
+	obj.data.isYearUser = "2";
 	obj.data.vipLuxuryExpire = "4102500561000";
 	obj.data.time = "4102500561000";
-  obj.data.vipExpire = "4102500561000";
+	obj.data.vipExpire = "4102500561000";
 	obj.data.vip3Expire = "4102500561000";
 	obj.data.vipmExpire = "4102500561000";
 	obj.data.vipOverSeasExpire = "4102500561000";
-	body = JSON.stringify(obj);
-}
-
-if ($request.url.indexOf(time) != -1) {
+};
+if ($request.url.indexOf(path2) != -1) {
 	obj.isVIPMAutoPay = 2;
 	obj.isVIPLuxAutoPay = 2;
-	body = JSON.stringify(obj);
-}
+};
 
 $done({body: JSON.stringify(obj)});
