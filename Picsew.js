f9ef21295eee11bfb49c6cb0b8ddba1af9064e6e
LTribeSkyx <60221182+LTribeSkyx@users.noreply.github.com>
2020-04-28 12:27:23 +0800

    Update

--- Picsew.js
+++ Picsew.js
@@ -14,7 +14,26 @@ hostname = buy.itunes.apple.com,
 */
 
 let obj = JSON.parse($response.body);
-obj.receipt.in_app = [{
+obj = {
+  "receipt": {
+    "receipt_type": "Production",
+    "adam_id": 0,
+    "app_item_id": 0,
+    "bundle_id": "com.sugarmo.ScrollClip",
+    "application_version": "3061",
+    "download_id": 0,
+    "version_external_identifier": 0,
+    "receipt_creation_date": "2020-01-01 00:00:00 Etc/GMT",
+    "receipt_creation_date_ms": "1588000000000",
+    "receipt_creation_date_pst": "2020-04-27 21:10:15 America/Los_Angeles",
+    "request_date": "2020-01-01 00:00:00 Etc/GMT",
+    "request_date_ms": "1588000000000",
+    "request_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
+    "original_purchase_date": "2001-01-01 00:00:00 Etc/GMT",
+    "original_purchase_date_ms": "1576200000000",
+    "original_purchase_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
+    "original_application_version": "3037",
+    "in_app": [{
       "quantity": "1",
       "product_id": "com.sugarmo.ScrollClip.pro",
       "transaction_id": "1000000000000000",
@@ -26,6 +45,10 @@ obj.receipt.in_app = [{
       "original_purchase_date_ms": "1587700000000",
       "original_purchase_date_pst": "2020-01-01 00:00:00 America/Los_Angeles",
       "is_trial_period": "false"
-    }];
+    }]
+  },
+  "status": 0,
+  "environment": "Production"
+};
 
 $done({body: JSON.stringify(obj)});
