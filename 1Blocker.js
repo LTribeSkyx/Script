82dda1a1e87da68f85d0f58362117020b8a93a25
LTribeSkyx <60221182+LTribeSkyx@users.noreply.github.com>
2020-11-14 23:28:27 +0800

    Update 1Blocker.js

--- 1Blocker.js
+++ 1Blocker.js
@@ -4,7 +4,7 @@
 Quantumult X 脚本:
 [rewrite_local]
 # 1Blocker Unlock （by LTribe）
-^https://api.revenuecat.com/v1/subscribers/* url script-response-body 1Blocker.js
+^https:\/\/api\.revenuecat\.com\/v1\/subscribers* url script-response-body 1Blocker.js
 
 [mitm]
 hostname = api.revenuecat.com,
@@ -16,32 +16,33 @@ let obj = JSON.parse($response.body);
 obj.subscriber = {
     "entitlements": {
       "premium": {
-        "expires_date": "2020-11-28T12:45:20Z",
+        "expires_date": "2030-01-01T01:01:01Z",
         "grace_period_expires_date": null,
         "product_identifier": "blocker.ios.subscription.yearly",
-        "purchase_date": "2020-11-14T12:45:20Z"
+        "purchase_date": "2020-01-01T01:01:01Z"
       }
     },
-    "first_seen": "2020-11-14T12:43:59Z",
-    "last_seen": "2020-11-14T12:43:59Z",
+    "first_seen": "2030-01-01T01:01:01Z",
+    "last_seen": "2030-01-01T01:01:01Z",
     "management_url": "itms-apps://apps.apple.com/account/subscriptions",
     "non_subscriptions": {},
-    "original_app_user_id": "0B14FE75-27E3-445C-96DC-D0B4DEB12254",
+    "original_app_user_id": "0099LK05-2799-489J-09CD-D9IJSEB09876",
     "original_application_version": "900",
-    "original_purchase_date": "2020-11-14T12:43:04Z",
+    "original_purchase_date": "2030-01-01T01:01:01Z",
     "other_purchases": {},
     "subscriptions": {
       "blocker.ios.subscription.yearly": {
         "billing_issues_detected_at": null,
-        "expires_date": "2020-11-28T12:45:20Z",
+        "expires_date": "2030-01-01T01:01:01Z",
         "grace_period_expires_date": null,
         "is_sandbox": false,
-        "original_purchase_date": "2020-11-14T12:45:21Z",
+        "original_purchase_date": "2020-01-01T01:01:01Z",
         "period_type": "trial",
-        "purchase_date": "2020-11-14T12:45:20Z",
+        "purchase_date": "2020-01-01T01:01:01Z",
         "store": "app_store",
         "unsubscribe_detected_at": null
       }
     }
  }
+
 $done({body: JSON.stringify(obj)});
